9b9649d853130c151f958210722bbe1222e76043
dawid.solecki <dawid@slickbeet.com>
2024-12-07 00:55:49 +0100

    proper structure

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Define a simple route
 app.get('/', (req, res) => {
   res.send('Hello, Render!');
 });
 
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} !!!`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
