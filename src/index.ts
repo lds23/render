30e6607b4406f228404d97bbe959e5b8170ccc97
dawid.solecki <dawid@slickbeet.com>
2024-12-08 18:02:37 +0100

    Untrack and ignore node_modules directory

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
+import dotenv from "dotenv";
 import { OpenAI } from "openai";
 
-const express = require('express');
+import express, { Request, Response, NextFunction } from 'express';
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Render!');
+// Define a route with properly typed parameters
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hello, TypeScript with Express!');
 });
 
-app.get('/ai', (req, res) => {
-  res.send('AI Req');
-});
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-
+dotenv.config();
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY, });
 
 async function getAIresp(prompt: string, sysCommand: string): Promise<string> {
@@ -26,6 +26,6 @@ async function getAIresp(prompt: string, sysCommand: string): Promise<string> {
         messages: [{ role: "system", content: sysCommand },
         { role: "user", content: prompt },]
     });
-    const resp = aiResp.choices[0].message?.content;
+    const resp = aiResp.choices[0].message!.content!;
     return resp;
 }
